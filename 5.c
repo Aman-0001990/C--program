617577222dbdfc796e2dfe31a7dc88da032e513b
Ch-MANASA19 <manasachimata19@gmail.com>
2025-09-30 19:05:33 +0530

    Update 5.c

--- 5.c
+++ 5.c
@@ -4,12 +4,8 @@ int main() {
     printf("Enter a positive integer: ");
     scanf("%d", &num);
     printf("Factors of %d are: \n%d", num,count);
-    for (i = 1; i <= num; ++i) {
-        if (num % i == 0) {
-            printf("%d ", i);
-            count++;
+    
             //printf("%d",count);
-        }
-    }
     return 0;
 }
+
