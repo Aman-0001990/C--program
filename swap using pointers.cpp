eaa7efefa2f285df62bbc89a3b4ea31d14d33172
Ch-MANASA19 <manasachimata19@gmail.com>
2025-09-30 12:12:10 +0530

    swap

---
+++ swap using pointers.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int swap(int *a, int *b){
+	 int temp=*a;
+	*a=*b;
+	*b=temp;
+	return 0;
+	
+}
+int main(){
+	int a,b,temp;
+	scanf("%d %d",&a,&b);
+	swap(&a,&b);
+//	printf("%d %d",a,b);
+/*	temp=*a;
+	*a=*b;
+	b=temp;/
+	printf("%d %d",a,b);*/
+	return 0;
+}
